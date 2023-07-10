c1033ae904a3b389b158821de1f1d8bfa918e407
Tirth <tirthsolanki29923@gmail.com>
2023-07-10 11:30:56 +0530

    cookie4

--- utils/features.js
+++ utils/features.js
@@ -12,13 +12,15 @@ export const sendToken = (user, res, message, statusCode) => {
   
     res
       .cookie("token", token, {
-        // ...cookieOptions,
-        httpOnly: true,
+        ...cookieOptions,
+        // httpOnly: true,
         expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
       })
       .json({
         success: true,
         message: message,
+        user:user,
+        token:token
       });
   };
 
