02b155dacec43486d705328f18d38a243e9c1804
Tirth <tirthsolanki29923@gmail.com>
2023-07-10 21:38:29 +0530

    cors3

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
 
 app.use(
     cors({
-        origin:true,
+        origin: "http://127.0.0.1:5173/",
         // credentials: true,
         methods:["GET","POST","PUT","DELETE"],
     }),
