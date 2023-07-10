914ce75e2f5ecc7fc372b7761fc5de0954722c85
Tirth <tirthsolanki29923@gmail.com>
2023-07-10 20:30:13 +0530

    cors

--- app.js
+++ app.js
@@ -20,11 +20,6 @@ app.use("*",
         credentials: true,
         methods:["GET","POST","PUT","DELETE"],
     }),
-    function(req, res, next) {  
-        res.header('Access-Control-Allow-Origin', req.headers.origin);
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-   }
 )
 
 // import all routes here 
